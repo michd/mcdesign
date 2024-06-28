a04e8846bcc62c236afca52f3c09654d88d93c37
Michael Duerinckx <mich@michd.me>
2024-06-28 10:21:40 +0100

    Fix deploy script to get rbenv env vars

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # Deployment script for on my server
+export RBENV_ROOT=/usr/local/rbenv/bin
+export PATH="$RBENV_ROOT/bin:$PATH"
 eval "$(rbenv init - )"
 bundle install
 touch tmp/restart.txt
